4d62bc3bb41c390eb8c413fda5d761a08440bed7
anayeaye <alexandra@developmentseed.org>
2021-07-30 11:29:15 -0600

    minor adjustments to hdf4 lambda

--- cumulus-tf/lambdas/hdf4-to-cog/src/main.py
+++ cumulus-tf/lambdas/hdf4-to-cog/src/main.py
@@ -24,12 +24,12 @@ config=dict(GDAL_NUM_THREADS="ALL_CPUS", GDAL_TIFF_OVR_BLOCKSIZE="128")
 output_profile = cog_profiles.get("deflate")
 output_profile["blockxsize"] = 256
 output_profile["blockysize"] = 256
-output_profile["count"] = len(modis_vi_config["variable_names"])
-output_profile["dtype"] = modis_vi_config["dtype"]
+# output_profile["count"] = len(modis_vi_config["variable_names"])
+# output_profile["dtype"] = modis_vi_config["dtype"]
 
 def generate_and_upload_cog(granule, file_staging_dir):
     """
-    Downloads granule hdf from #3, transforms specified variables/sub datasets to COG, 
+    Downloads granule hdf from S3, transforms specified variables/sub datasets to COG, 
     publishes back to same S3 staging area.
 
     @param granule: granule JSON object parsed from Cumulus Message Adapter event input
@@ -84,18 +84,18 @@ def generate_and_upload_cog(granule, file_staging_dir):
                         nodata=sub_dst.nodata
                     )
 
-                    # Confirm that these metadata are consistent in output profile and add if this is the first dataset/band
-                    for key in sub_dst_meta.keys():
-                        if key in output_profile.keys():
-                            assert(sub_dst_meta[key] == output_profile[key])
-                        else:
-                            output_profile[key] = sub_dst_meta[key]
+                    # # Confirm that these metadata are consistent in output profile and add if this is the first dataset/band
+                    # for key in sub_dst_meta.keys():
+                    #     if key in output_profile.keys():
+                    #         assert(sub_dst_meta[key] == output_profile[key])
+                    #     else:
+                    #         output_profile[key] = sub_dst_meta[key]
 
                     # Read band array and scale if needed
                     band_data = sub_dst.read(1)
                     if len(sub_dst.scales):
                         scale_factor = sub_dst.scales[0]
-                        band_data = np.where(band_data != output_profile["nodata"], band_data / scale_factor, sub_dst_meta["nodata"])
+                        band_data = np.where(band_data != sub_dst_meta["nodata"], band_data / scale_factor, sub_dst_meta["nodata"])
                     
                     # Add band to output
                     bands.append({
@@ -105,7 +105,7 @@ def generate_and_upload_cog(granule, file_staging_dir):
         # End subdatasets
 
         # Write to local
-        with rasterio.open(output_filename, 'w', **output_profile) as outfile:
+        with rasterio.open(output_filename, **output_profile) as outfile:
             print(f"Writing {len(bands)} bands")
             for idx, band in enumerate(bands):
                 outfile.write(band["data"], idx+1)
@@ -120,9 +120,11 @@ def generate_and_upload_cog(granule, file_staging_dir):
                 output_profile,
                 config=config,
                 overview_resampling="nearest",
-                quiet=True
+                # quiet=True,
+                use_cog_driver=True,
+                dtype=np.float32
             ) 
-            assert cog_validate(output_filename)
+            assert cog_validate(output_filename)[0]
 
             # Upload to S3
             print(f"Uploading {output_s3_path}")
